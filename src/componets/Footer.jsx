550eb8ccd59ad97e834e9adc30ea685b2902b3b6
ArielMarno <arielmarno@outlook.com>
2023-10-04 20:02:34 -0300

    fix(medidas de iconos contacto, altura minima de componentes)

--- src/componets/Footer.jsx
+++ src/componets/Footer.jsx
@@ -13,8 +13,8 @@ const Footer = () => {
         <h2>Calculando</h2>
         </div>
           <div className="social">
-            <a href="https://www.linkedin.com/in/ariel-alejandro-marchesano-97729b26b/" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="linkedin icon"/></a>
-            <a href="https://github.com/ArielMarno?tab=repositories" target="_blank" rel="noopener noreferrer"><img src={github} alt="github icon"/></a>
+            <a href="https://www.linkedin.com/in/arielmarchesano/" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="linkedin icon"/></a>
+            <a href="https://github.com/ArielMarno" target="_blank" rel="noopener noreferrer"><img src={github} alt="github icon"/></a>
             <a href="mailto: arielmarno@outlook.com"><img src={outlook} alt="outlook icon"/></a>
           </div>
           <div className="developer">
